ce44f1d63cc752d06b7b9d4d391730a1842333e1
Ubuntu <ubuntu@ip-172-31-40-148.us-east-2.compute.internal>
2020-04-24 18:51:15 +0000

    modified App.js to render class and name

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <h1>CFD-Tim Rogers ~ Project 3</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
